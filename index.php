6f53884554b9b1efab77e073bb380749fea8e00e
agustin <agustin.caceres96@hotmail.com>
2018-10-08 13:50:18 -0300

    test

--- index.php
+++ index.php
@@ -3,11 +3,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//require_once("Config/config.php");
+require_once("Config/config.php");
 require_once("Config/constantes.php");
 require_once("Config/autoload.php");
-//require_once("Config/Request.php");
-//require_once("Config/Router.php");
+require_once("Config/Request.php");
+require_once("Config/Router.php");
 
 
 use Config\autoload as autoload;
